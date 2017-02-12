df6c1aa1f1949d440d5ca41969b0a3a0b8d721f0
AlanPadi95 <alanpadierna9595@gmail.com>
2017-02-12 13:28:20 +0100

    Modificación de la contraseña sin SpringBoot

--- src2/asw/business/user/UpdatePassword.java
+++ src2/asw/business/user/UpdatePassword.java
@@ -2,22 +2,26 @@ package asw.business.user;
 
 import asw.business.Command;
 import asw.business.exception.BusinessException;
+import asw.model.Usuario;
 
 public class UpdatePassword implements Command {
 
 	private String contraseñaAntigua;
 	private String contraseñaNueva;
+	private Usuario user;
 
-	public UpdatePassword(String contraseñaAntigua, String contraseñaNueva) {
+	public UpdatePassword(Usuario u, String contraseñaAntigua, String contraseñaNueva) {
 		super();
 		this.contraseñaAntigua = contraseñaAntigua;
 		this.contraseñaNueva = contraseñaNueva;
+		this.user = u;
 	}
 
 	@Override
-	public Object execute() throws BusinessException {
-		
-		return null;
+	public Usuario execute() throws BusinessException {
+		user.updateContraseña(contraseñaAntigua, contraseñaNueva);
+		return user;
+
 	}
 
 }
--- src2/asw/ui/action/user/UpdatePasswordAction.java
+++ src2/asw/ui/action/user/UpdatePasswordAction.java
@@ -1,14 +1,17 @@
 package asw.ui.action.user;
 
+import asw.business.user.UpdatePassword;
+import asw.model.Usuario;
 import asw.ui.action.Action;
 
-public class UpdatePasswordAction implements Action{
+public class UpdatePasswordAction implements Action {
 
 	private String contraseñaAntigua;
 	private String contraseñaNueva;
+
 	@Override
 	public void execute() throws Exception {
-	
+		Usuario user = new UpdatePassword(u, contraseñaAntigua, contraseñaNueva).execute();
 	}
 
 }
