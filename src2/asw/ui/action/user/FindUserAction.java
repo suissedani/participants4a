08a59af59b4360f4002b728504b11439fdbc9add
DANIELTOIMIL\Daniel <djlt93@gmail.com>
2017-02-13 16:50:08 +0100

    JSON en FindUserAction

--- src2/asw/ui/action/user/FindUserAction.java
+++ src2/asw/ui/action/user/FindUserAction.java
@@ -1,5 +1,7 @@
 package asw.ui.action.user;
 
+import org.json.JSONObject;
+
 import asw.business.user.FindUser;
 import asw.model.Usuario;
 import asw.ui.action.Action;
@@ -8,9 +10,18 @@ public class FindUserAction implements Action {
 
 	private String email;
 	private String password;
+	private JSONObject json;
+	
+	public FindUserAction(JSONObject json, String email, String password) {
+		this.email=email;
+		this.password = password;
+		this.json = json;
+	}
 
 	@Override
 	public void execute() throws Exception {
+		String email = json.getString("email");
+		String password = json.getString("password");
 		Usuario user = new FindUser(email, password).execute();
 	}
 
