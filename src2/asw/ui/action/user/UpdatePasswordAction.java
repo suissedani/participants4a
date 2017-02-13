b582679ffe3d48e20530c12c5606d4d6ee876c9c
DANIELTOIMIL\Daniel <djlt93@gmail.com>
2017-02-13 16:53:11 +0100

    JSON en UpdatePassword

--- src2/asw/ui/action/user/UpdatePasswordAction.java
+++ src2/asw/ui/action/user/UpdatePasswordAction.java
@@ -1,5 +1,7 @@
 package asw.ui.action.user;
 
+import org.json.JSONObject;
+
 import asw.business.user.UpdatePassword;
 import asw.model.Usuario;
 import asw.ui.action.Action;
@@ -8,9 +10,18 @@ public class UpdatePasswordAction implements Action {
 
 	private String passAntigua;
 	private String passNueva;
+	private JSONObject json;
+	
+	public UpdatePasswordAction(JSONObject json, String passAntigua, String passNueva) {
+		this.passAntigua = passAntigua;
+		this.passNueva = passNueva;
+		this.json = json;
+	}
 
 	@Override
 	public void execute() throws Exception {
+		String passAntigua= json.getString("passAntigua");
+		String passNueva = json.getString("passNueva");
 		Usuario user = new UpdatePassword(u, passAntigua, passNueva).execute();
 	}
 
