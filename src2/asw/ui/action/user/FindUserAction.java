942ef3cb86b56f919700685de6409c9c14eb2a6a
DANIELTOIMIL\Daniel <djlt93@gmail.com>
2017-02-14 13:00:27 +0100

    no message

--- src2/asw/ui/action/user/FindUserAction.java
+++ src2/asw/ui/action/user/FindUserAction.java
@@ -1,28 +1,40 @@
 package asw.ui.action.user;
 
+import org.apache.catalina.User;
 import org.json.JSONObject;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import asw.business.user.FindUser;
 import asw.model.Usuario;
 import asw.ui.action.Action;
+import ch.qos.logback.core.net.LoginAuthenticator;
 
 public class FindUserAction implements Action {
 
 	private String email;
 	private String password;
-	private JSONObject json;
 	
-	public FindUserAction(JSONObject json, String email, String password) {
+	public FindUserAction(String email, String password) {
 		this.email=email;
 		this.password = password;
-		this.json = json;
 	}
 
 	@Override
+	@RequestMapping(value="/validarse", method= RequestMethod.POST)
 	public void execute() throws Exception {
-		String email = json.getString("email");
-		String password = json.getString("password");
+		String email = email;
+		String password = password;
 		Usuario user = new FindUser(email, password).execute();
+		println(email + " " + password);
 	}
 
 }
